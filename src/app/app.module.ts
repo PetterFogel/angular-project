0ce54b5147242cedd8652478fcff38acd8cfe149
Petter <petter.fogel@medieinstitutet.se>
2021-02-12 14:23:54 +0100

    Add todo-list component and layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FormComponent } from './components/form/form.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FormComponent,
+    TodoListComponent,
   ],
   imports: [
     BrowserModule,
