4e3c854c06eb0e914bd5d7c52ff9bfbbc41aea5e
Petter <petter.fogel@medieinstitutet.se>
2021-02-17 13:17:38 +0100

    Typed inputValue as string.

--- src/app/components/todo-form/todo-form.component.ts
+++ src/app/components/todo-form/todo-form.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
-
 @Component({
   selector: 'app-todo-form',
   templateUrl: './todo-form.component.html',
   styleUrls: ['./todo-form.component.css']
 })
 export class TodoFormComponent implements OnInit {
-inputValue = "";
+inputValue: string = "";
 @Output() buttonClicked = new EventEmitter<string>();
-
 
   constructor() { }
 
@@ -19,7 +17,7 @@ inputValue = "";
   getValue(val: any) {
     this.inputValue = val.target.value;
   }
-
+  
   fetchInputValue() {
     this.buttonClicked.emit(this.inputValue);
   }
